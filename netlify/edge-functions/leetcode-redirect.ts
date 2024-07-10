c571da06c58334613ebe2898c2642f0343acb081
GOKULA KANNAN V <gokulakannanv04@gmail.com>
2024-07-10 12:42:14 +0530

    Automated commit - 2024-07-10 12:42:14

--- netlify/edge-functions/leetcode-redirect.ts
+++ netlify/edge-functions/leetcode-redirect.ts
@@ -12,13 +12,11 @@ export default async function handler() {
     const next = new Date(daily.date).getTime() + 24 * 60 * 60 * 1000;
     const ttl = Math.floor((next - now) / 1000);
 
-    const response = new Response(null, {
+    return new Response(null, {
         status: 302,
         headers: {
-            Location: "https://leetcode.com" + daily.link,
+            Location: `https://leetcode.com${daily.link}`,
             "Cache-Control": `public, s-maxage=${ttl}`,
         },
     });
-
-    return response;
 }
